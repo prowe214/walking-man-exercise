16371be9525f5bd76433b80815e47bbe48e2ac2c
Paul Cullen Rowe <prowe214@gmail.com>
2015-05-21 16:23:05 -0600

    pushing to esteube

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 var daMan = document.getElementById('gif');
-
+var interval;
+var winWidth = document.body.clientWidth
 currentPos = 0;
 daMan.style.left = currentPos + 'px';
 
-window.addEventListener('click', function() {
-  currentPos += 20;
+
+(function moveDaMan() {
+  alert('yo');
+  interval = setInterval(cripWalk, 200);
+  while (winWidth < daMan.style.left) {
+    daMan.className = 'right';
+  };
+})();
+
+function cripWalk() {
+  currentPos += 10;
   daMan.style.left = currentPos + 'px';
-});
-
-
-
-
-/* var interval;
-
-function moveDaMan() {
-  interval = setInterval(cripWalk, 1000)
-} */
+};
